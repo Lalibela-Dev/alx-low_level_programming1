a223e2c2ad86790638766ef9e722a95ab51e2258
Lalibela-Dev <abeventss123@gmail.com>
2022-09-15 10:17:02 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - funtion that print all numbers from 0 to 9
+ *
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	int n;
+	if (n - 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
